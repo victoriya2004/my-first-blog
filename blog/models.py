4e7030668e0157ed1aa89e33b0b8297e424dbf87
victoriya2004 <victoriya_n2004@mail.ru>
2016-11-15 17:18:26 +0200

    comments

--- blog/models.py
+++ blog/models.py
@@ -10,6 +10,9 @@ class Post(models.Model):
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+	
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
 
     def publish(self):
         self.published_date = timezone.now()
@@ -17,3 +20,17 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
